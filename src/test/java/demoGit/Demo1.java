3ae4b6d43565291655e9445ed1651bd38185c4bc
Kavyashree <skavyashree96@gmail.com>
2022-03-12 13:23:02 +0530

    demo1 first commit

---
+++ src/test/java/demoGit/Demo1.java
@@ -0,0 +1,16 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void git()
+	{
+		System.out.println("kavya - first commit");
+		
+		
+		
+	}
+
+}
